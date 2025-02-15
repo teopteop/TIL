f2b3d4de5a3dd5f3c36c6e8d800cafb7dec2df33
teopteop <teopteop0718@gmail.com>
2025-02-16 02:22:34 +0900

    스프링 빈

---
+++ Sping/demo/src/test/java/com/example/demo/XML/XmlAppConText.java
@@ -0,0 +1,21 @@
+package com.example.demo.XML;
+
+import com.example.demo.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class XmlAppConText {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new
+                GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
+
+
